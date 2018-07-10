6ba8321dc9b12344246a3c0eda5d4d7e0e81e890
Srinivasan Rangarajan <srinivasanr@gmail.com>
2018-07-10 22:49:21 +0530

    Add code to return Hello World at /

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-'use strict';
+const express = require('express')
+const sls = require('serverless-http')
+const app = express() // create the app
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+// Just an API to display Hello World!
+app.get('/', async (req, res, next) => {
+    res.status(200).send('Hello World!')
+})
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+module.exports.server = sls(app)
